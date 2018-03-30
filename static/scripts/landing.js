a034119f80a4864bc6876ec4180a7cc5af318a6f
Emma O'Neill <emmaruthoneill@gmail.com>
2018-03-30 15:04:56 -0700

    doing searchbar stuff

--- static/scripts/landing.js
+++ static/scripts/landing.js
@@ -16,6 +16,23 @@ var timeTimeout = 5000;
 
 console.log(restaurants);
 
+// smooth scrolling down to nav elements
+$(document).on('click', 'a[href^="#"]', function(e) {
+    // target element id
+    var id = $(this).attr('href');
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+        return;
+    }
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+    // top position relative to the document
+    var pos = $(id).offset().top - 80;
+    // animated top scrolling
+    $('body, html').animate({scrollTop: pos});
+});
+
 // search bar code -------------------------------------------------------------
 // IMPORTANT: Keep this updated with filter options
 function getFilterList() {
