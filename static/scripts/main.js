058e4f37278d9f797c7671c75ea94ab8985c1b68
ewagstaff <evanjwagstaff@gmail.com>
2018-04-05 15:29:31 -0700

    Tightening unseen restaurant logic

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -29,18 +29,22 @@ if($('.swiper-slide').length === 1){
 // We've arrived on an article, so save a cookie that says so
 // Get existing cookie
 var cookieValue = getCookie("sfc_top100_2018");
-console.log(cookieValue, "cookieValue");
 if (!cookieValue){
 	cookieValue = "";
-}
-
+} 
 // If the slug is not present in the string
 if (cookieValue.indexOf(restaurant.Slug) == -1){
+	// If it's not the first name, add a spacer
+	if (cookieValue){
+		cookieValue += "|";
+	} 
 	// Add to existing cookie
-	cookieValue += "|" + restaurant.Slug;
+	cookieValue += restaurant.Slug;
 	// Max cookie expiration
 	setCookie("sfc_top100_2018", cookieValue, 2147483647);
 }
+
+console.log("cookieValue", cookieValue, cookieValue.split("|"));
 
 // Get current strings in cookie and report back
 var cookiesNumber = 100 - cookieValue.split("|").length;
@@ -62,6 +66,42 @@ if (cookiesNumber > 10){
 
 console.log(restaurant, restaurants);
 
+var findUnseenRestaurant = function(index, cookie, alreadySelected){
+	// Loop through and find a restaurant the user hasn't seen yet
+	var foundRest = false;
+	var countIndex = index;
+	var catchCount = 0;
+	while (!foundRest){
+		// Wrap back around if we're at 100
+		if (countIndex == 100){
+			countIndex = 0;
+		}
+
+		console.log(countIndex);
+		console.log(restaurants[countIndex]);
+
+		if (cookie.indexOf(restaurants[countIndex].Slug) == -1){
+			// Check if it's already selected
+			if (alreadySelected.indexOf(restaurants[countIndex]) == -1){
+				// This means the user hasn't visited, let's return
+				foundRest = true;
+				return restaurants[countIndex];
+			}
+		} else {
+			// Catch if we go over 100 and still haven't found anything
+			// Which could happen if they've read it all!
+			if (catchCount > 100){
+				foundRest = true;
+				return null;
+			}
+		}
+
+		// Increment
+		countIndex++;
+		catchCount++;
+	}
+}
+
 // Find restaurant that matches the current name
 var thisRestaurant = restaurants.find(function(element) {
   return element.Name == restaurant.Name;
@@ -72,32 +112,41 @@ var thisRestaurantIndex = restaurants.indexOf(thisRestaurant);
 
 // Related restaurants are the next 3
 var relatedRestaurants = [];
-relatedRestaurants[0] = restaurants[thisRestaurantIndex+1];
-relatedRestaurants[1] = restaurants[thisRestaurantIndex+2];
-relatedRestaurants[2] = restaurants[thisRestaurantIndex+3];
+for (var r = 0; r < 3; r++){
+	relatedRestaurants[r] = findUnseenRestaurant(thisRestaurantIndex, cookieValue, relatedRestaurants); 
+}
+
 console.log(relatedRestaurants);
 
 // Set values on HTML
 $("#related-rest .wrap").each(function(index){
-  // Get URL with no query or hash (or trailing slash)
-  var fullUrl = location.href.split('#')[0].split('?')[0].slice(0, -1);
-  // Get array based on slashes
-  fullUrl = fullUrl.split("/");
-  // Remove the restaurant path and rejoin
-  fullUrl.pop();
-  fullUrl = fullUrl.join("/"); 
-  // Set new path using slug
-  var finalUrl = fullUrl+"/"+relatedRestaurants[index].Slug;
-  $(this).find("a").attr("href", finalUrl);
-  $(".rtitle .next-link").attr("href", finalUrl);
-  // Set image URL
-  if (relatedRestaurants[index].wcm_img){
-    $(this).find("img").attr("src", "https://ww3.hdnux.com/photos/60/22/02/"+relatedRestaurants[index].wcm_img.split(" ")[0]+"/7/premium_gallery_landscape.jpg");
-  }
-  // Set name
-  $(this).find(".name div").text(relatedRestaurants[index].Name);
-  // Set region
-  $(this).find(".info").text(relatedRestaurants[index].Region + " | " + relatedRestaurants[index].SubRegion);
+	// Only create if it exists
+	if (relatedRestaurants[index] != null){
+		// Get URL with no query or hash (or trailing slash)
+	  var fullUrl = location.href.split('#')[0].split('?')[0].slice(0, -1);
+	  // Get array based on slashes
+	  fullUrl = fullUrl.split("/");
+	  // Remove the restaurant path and rejoin
+	  fullUrl.pop();
+	  fullUrl = fullUrl.join("/"); 
+	  // Set new path using slug
+	  var finalUrl = fullUrl+"/"+relatedRestaurants[index].Slug;
+	  $(this).find("a").attr("href", finalUrl);
+	  // Set next link as the text link
+	  if (index == 0){
+	  	$(".rtitle .next-link").attr("href", finalUrl);
+	  }
+	  // Set image URL
+	  if (relatedRestaurants[index].wcm_img){
+	    $(this).find("img").attr("src", "https://ww3.hdnux.com/photos/60/22/02/"+relatedRestaurants[index].wcm_img.split(" ")[0]+"/7/premium_gallery_landscape.jpg");
+	  }
+	  // Set name
+	  $(this).find(".name div").text(relatedRestaurants[index].Name);
+	  // Set region
+	  $(this).find(".info").text(relatedRestaurants[index].Region + " | " + relatedRestaurants[index].SubRegion);
+	} else {
+		$(this).remove();
+	}
 });
 
 
